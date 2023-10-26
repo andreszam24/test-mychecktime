4ad0de7f1bb7d0290e7983fe05b3fb5a98fc9f94
Carolina Patino <layna.designer@gmail.com>
2023-10-26 10:44:10 -0500

    creacion de componente toast y internet-status

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,32 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { Network, ConnectionStatus} from '@capacitor/network';
+import {InternetStatusComponent} from '../internet-status/internet-status.component'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, InternetStatusComponent]
 })
 export class HomePage implements OnInit {
-
-  networkStatus!:ConnectionStatus
 
   constructor() { }
 
   ngOnInit() {
 
-    if(Network){
-      Network.getStatus().then((status)=>{
-        this.networkStatus=status
-      })
-    }
-
-    Network.addListener('networkStatusChange', (status) => {
-      this.networkStatus = status;
-    });
 
   } 
+
 }
