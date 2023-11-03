93b27415f1302d4ea5584419b261e9d7935a81a9
Zambrano <andreszambrano24@gmail.com>
2023-11-03 23:48:14 +0100

    se adiciona pagina login en blanco

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { InternetStatusComponent } from '../../components/internet-status/internet-status.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, InternetStatusComponent]
+})
+export class LoginPage implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(){}
+
+}
