2b434d872c329edfde1b883e9415b10a3d388993
Zambrano <andreszambrano24@gmail.com>
2023-11-03 18:48:22 +0100

    HU-Lectura QR: Se implementa y se deja func ejempl

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, AlertController } from '@ionic/angular';
 import { InternetStatusComponent } from '../../components/internet-status/internet-status.component';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,40 @@ import { InternetStatusComponent } from '../../components/internet-status/intern
   imports: [IonicModule, CommonModule, FormsModule, InternetStatusComponent]
 })
 export class LoginPage implements OnInit {
+  isSupported = false;
+  barcodes: Barcode[] = [];
 
-  constructor(){}
+  constructor(private alertController: AlertController){}
 
-  ngOnInit(){}
+  ngOnInit(){
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
+  async scan(): Promise<void> {
+    (window.document.querySelector('ion-app') as HTMLElement).classList.add('cameraView');
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+    (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
 }
