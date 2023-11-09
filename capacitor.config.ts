5d8f12a72fa1c5211a455379b95aaf3a48975b51
Zambrano <andreszambrano24@gmail.com>
2023-11-09 20:19:51 +0100

    cambio de nombre de la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app_movil_ionic',
+  appName: 'MyCheckTime',
   webDir: 'www',
   bundledWebRuntime: false
 };
