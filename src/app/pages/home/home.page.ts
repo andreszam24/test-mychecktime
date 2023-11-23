d4fa20e6e3d289189f8b391f1e75ec3c1c8e386f
Carolina Patino <layna.designer@gmail.com>
2023-11-23 00:11:53 -0500

    error logico en mostrat todos los pacientes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,6 +5,13 @@ import { IonicModule } from '@ionic/angular';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton,IonCard,IonCardContent,IonList,IonItem,IonItemSliding, IonItemOption, IonItemOptions,IonImg} from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import {InternetStatusComponent} from '../../components/internet-status/internet-status.component';
+import { AuthService } from '../../services/auth.service';
+import { Patient } from '../../models/patient.model';
+import { PatientService } from 'src/app/services/patient.service';
+import { of, catchError } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -14,10 +21,34 @@ import {InternetStatusComponent} from '../../components/internet-status/internet
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton,IonCard,IonCardContent,IonList,IonItem,IonItemSliding, IonItemOption, IonItemOptions,IonImg,IonicModule, FormsModule, InternetStatusComponent, CommonModule],
 })
 export class HomePage implements OnInit {
+  patientsLis: Patient[] = [];
+  clinicName:string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private authService: AuthService, private patientService:PatientService ) { }
 
   ngOnInit() {
+    this.extractUserData();
+    this.getAllPatients();
+  }
+
+  private extractUserData(){
+    return this.authService.getUser().subscribe(
+      userData => {
+        this.clinicName = userData.account.clinics[0].name;
+      }
+    )
+  }
+
+  private getAllPatients(){
+    this.patientService.getAllPatients().pipe(
+      catchError((error) => {
+        console.log('Error', error);
+        return of(null);
+      })
+    ).subscribe((patients)=>{
+      this.patientsLis = JSON.parse(JSON.stringify(patients));
+      console.log('patientsLis', this.patientsLis);
+    });
   }
 
   public goToPatientIntake(){
--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -3,8 +3,8 @@ import { AuthService } from './auth.service';
 import { URLPatients, URLPatientsByDni, headers } from '../resources/urls.resource';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Patient } from '../models/patient.model';
-import { map, switchMap } from 'rxjs/operators';
-import { BehaviorSubject, Observable, from, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { Observable, from, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -14,49 +14,25 @@ export class PatientService {
 
     constructor(private http: HttpClient) { }
 
-    /*  getAllPatients(): Observable<Array<Patient>> {
-        return this.http.get(URLPatients + AuthService.getTokenParams(), {headers})
-          .map( (response) => {
-            const patients: Patient[] = JSON.parse(JSON.stringify(response));
+    getAllPatients(): Observable<Array<Patient>> {
+      return this.http.get(URLPatients + AuthService.getTokenParams(), { headers, observe: 'response' }).pipe(
+        map((response: HttpResponse<any>) => {
+          if (response.status === 200) {
+            const patients: Patient[] = response.body; 
             return patients;
-          }, (error) => {
-            console.log('Error consumiendo servicio para obtener los pacientes: ' + error);
+          } else {
+            console.log('Error consumiendo servicio para obtener los pacientes: ' + response.statusText);
             return [];
           }
-          );
-      }*/
+        }), catchError((err, caught) => {
+          console.error('Error logico en getAllPatients', err, caught);
+          return of([]);
+        })
 
-    getPatientById(id: number) {
-        const urlGetById = `${URLPatients}/${id}`;
-
-        return this.http.get(urlGetById + AuthService.getTokenParams(), { headers, observe: 'response' }).pipe(
-            map((response: HttpResponse<any>) => {
-                if (response.status === 200) {
-                    return response.body;
-                } else {
-                    console.log('usuario no autorizado:', response.status);
-                    return of(null);
-                }
-
-            }), switchMap((data) => {
-                return data;
-            })
-        );
+      );
     }
 
-    /*searchByName(text: string): Observable<Patient> {
-      const urlGetPatients = `${URLPatientsByName}/${text}`;
-  
-      return this.http.get(urlGetPatients + AuthService.getTokenParams(), httpOptions)
-      .map((response) => {
-        const patient : Patient = JSON.parse(JSON.stringify(response));
-        return patient;
-      }, (error) => {
-        console.log('Error consumiendo el servicio para obtener el paciente por nombre: ' + error);
-        Observable.throw(error);
-      }); 
-    }
-  */
+   
     searchByDni(text: string) {
         const urlGetPatients = `${URLPatientsByDni}/${text}`;
         return this.http.get(urlGetPatients + AuthService.getTokenParams(), { headers, observe: 'response' }).pipe(
@@ -75,16 +51,5 @@ export class PatientService {
         );
 
     }
-    /*
-      createPatient(newPatient: Patient): Observable<Patient> {
-    
-        return this.http.post(URLPatients + AuthService.getTokenParams(), newPatient, httpOptions)
-          .map((response) => {
-              const patient : Patient = JSON.parse(JSON.stringify(response));
-              return patient;
-          },(error) => {
-            console.log('Error consumiendo el servicio para crear un paciente: ' + error);
-            Observable.throw(error);
-          });
-      }*/
+  
 }
